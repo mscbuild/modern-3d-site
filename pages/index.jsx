46c99dcdb5cc205d15d197bd5cfa88629fcd090b
JR <180821520+mscbuild@users.noreply.github.com>
2025-07-28 11:36:36 +0300

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -32,10 +32,7 @@ const Home = () => {
             exit="hidden"
             className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16"
           >
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate,
-            exercitationem harum, quia nulla temporibus deleniti libero veniam
-            vero beatae numquam ducimus illum ab similique ipsam tempore fugit
-            quod laudantium debitis.
+           I create beautiful, responsive websites and applications that help businesses grow and succeed in the digital world.
           </motion.p>
 
           {/* btn */}
