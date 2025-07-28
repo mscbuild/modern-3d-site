a48209a6bd4f073b498330887eadcc7fa495c248
JR <180821520+mscbuild@users.noreply.github.com>
2025-07-28 11:44:28 +0300

    Update index.jsx

--- pages/services/index.jsx
+++ pages/services/index.jsx
@@ -31,8 +31,7 @@ const Services = () => {
               exit="hidden"
               className="mb-4 max-w-[400px] mx-auto lg:mx-0"
             >
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
-              tempora eligendi, quisquam tempore sint reiciendis.
+              Comprehensive digital solutions for your business.
             </motion.p>
           </div>
 
