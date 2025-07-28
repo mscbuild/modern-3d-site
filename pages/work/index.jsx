36fca550e4ffae4feb3bf111278ab8e65955bd88
JR <180821520+mscbuild@users.noreply.github.com>
2025-07-28 11:41:55 +0300

    Update index.jsx

--- pages/work/index.jsx
+++ pages/work/index.jsx
@@ -29,8 +29,7 @@ const Work = () => {
               exit="hidden"
               className="mb-4 max-w-[400px] mx-auto lg:mx-0"
             >
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
-              tempora eligendi, quisquam tempore sint reiciendis.
+             My approach combines technical expertise with creative design to deliver exceptional user experiences that drive results for businesses.
             </motion.p>
           </div>
 
