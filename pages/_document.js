e6c9fb1bdc8617e138112c9e7ba99558aa40a3fd
JR <180821520+mscbuild@users.noreply.github.com>
2025-07-29 15:25:59 +0300

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -5,9 +5,28 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          {/* Базовая конфигурация */}
           <meta charSet="UTF-8" />
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+          {/* SEO */}
+          <meta name="description" content="Portfolio" />
+          <meta name="keywords" content="Portfolio" />
+          <meta name="author" content="Mscbuild" />
+
+          {/* Open Graph / Facebook */}
+          <meta property="og:title" content="Alex Johnson | Portfolio" />
+          <meta property="og:description" content="Alex Johnson | Portfolio" />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://example.com/" />
+          <meta property="og:image" content="https://example.com/preview.jpg" />
+
+          {/* Twitter Cards */}
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:title" content="Заголовок для Twitter" />
+          <meta name="twitter:description" content="Описание для Twitter" />
+          <meta name="twitter:image" content="https://example.com/preview.jpg" />
         </Head>
         <body>
           <Main />
