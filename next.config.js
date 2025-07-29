bed901c0b306a328050491f4acc656559abc4775
JR <180821520+mscbuild@users.noreply.github.com>
2025-07-29 16:53:21 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -14,7 +14,7 @@ const nextConfig = {
               script-src 'self' 'unsafe-inline' 'unsafe-eval';
               style-src 'self' 'unsafe-inline';
               img-src 'self' data:;
-              font-src 'self';
+              font-src 'none';
               connect-src 'self';
               frame-ancestors 'none';
               object-src 'none';
