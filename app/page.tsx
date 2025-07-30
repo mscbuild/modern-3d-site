5406a0bf4348a42510f688e702f304f8071efdd5
JR <180821520+mscbuild@users.noreply.github.com>
2025-07-30 21:16:40 +0300

    Delete app directory

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-// app/page.tsx
-import { cookies } from 'next/headers'
-
-export async function getSessionData(req) {
-  const encryptedSessionData = cookies().get('session')?.value
-
-  return encryptedSessionData ? JSON.parse(decrypt(encryptedSessionData)) : null
-}
