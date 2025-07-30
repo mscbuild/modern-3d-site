9c1a768fdd4573a1168b61adfa404c9466bcb2af
JR <180821520+mscbuild@users.noreply.github.com>
2025-07-30 21:32:45 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -12,7 +12,7 @@ const nextConfig = {
             value: `
               upgrade-insecure-requests;
               form-action 'self';
-              font-src  'https';
+              font-src  'self';
               default-src 'self';
               script-src 'self' 'unsafe-inline' 'unsafe-eval';
               style-src 'self' 'unsafe-inline';
