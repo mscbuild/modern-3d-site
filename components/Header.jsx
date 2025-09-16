7b4879b76691350d6b962d85319a6f1964bed2bb
JR <180821520+mscbuild@users.noreply.github.com>
2025-09-16 09:15:18 +0300

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -9,14 +9,7 @@ const Header = () => {
       <div className="container mx-auto">
         <div className="flex flex-col lg:flex-row justify-between items-center gap-y-6 py-8">
           {/* logo */}
-          <Link href="/">
-            <Image
-              alt="logo"
-              width={220}
-              height={48}
-              unoptimized
-              priority
-            />
+          <Link>
           </Link>
 
           {/* socials */}
