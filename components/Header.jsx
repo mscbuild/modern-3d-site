0bfba30c610b7479c81158ceedb1d722cd329d8e
JR <180821520+mscbuild@users.noreply.github.com>
2025-09-16 09:09:17 +0300

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
           {/* logo */}
           <Link href="/">
             <Image
-              src="/logo.svg"
+              src="#"
               alt="logo"
               width={220}
               height={48}
