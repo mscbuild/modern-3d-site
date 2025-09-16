3aae87974acea33668c27dd22476dfc4b7287991
JR <180821520+mscbuild@users.noreply.github.com>
2025-09-16 09:20:51 +0300

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
       <div className="container mx-auto">
         <div className="flex flex-col lg:flex-row justify-between items-center gap-y-6 py-8">
           {/* logo */}
-          <Link href="/logo.svg">
+          <Link href="public/logo.svg">
             <Image
               alt="logo"
               width={220}
